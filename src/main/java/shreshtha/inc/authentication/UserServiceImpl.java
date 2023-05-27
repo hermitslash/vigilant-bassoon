131817f943de04a167ccdb1599946766d97e261e
hermitslash <chinmai.bharadwaj@ymail.com>
2023-05-27 15:49:52 +0530

    added async calls

--- src/main/java/shreshtha/inc/authentication/UserServiceImpl.java
+++ src/main/java/shreshtha/inc/authentication/UserServiceImpl.java
@@ -2,6 +2,7 @@ package shreshtha.inc.authentication;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -22,44 +23,51 @@ class UserServiceImpl implements UserService {
     private final AuthServiceGateway authServiceGateway;
     private final AuthenticationManager authenticationManager;
     private final TokenUtils tokenUtils;
+    private final AsyncTaskExecutor asyncTaskExecutor;
 
 
     @Override
     public AppUserDto loggedInUser() {
-        Optional<JpaUser> foundUser = authServiceGateway.loggedInUser();
-        if (foundUser.isPresent()) {
-            return foundUser.map(au -> new AppUserDto(au.getUserId(), au.getUsername(),
-                    au.getFirstName(), au.getLastName(), au.getEmailAddress(),
-                    au.getPhoneNumber(), au.getCompanyName(), au.getAuthorities().stream().map(AppRole::getAuthority).collect(Collectors.joining(","))
-            )).get();
-        }
-        return null;
+        return asyncTaskExecutor.submitCompletable(() -> {
+            Optional<JpaUser> foundUser = authServiceGateway.loggedInUser();
+            if (foundUser.isPresent()) {
+                return foundUser.map(au -> new AppUserDto(au.getUserId(), au.getUsername(),
+                        au.getFirstName(), au.getLastName(), au.getEmailAddress(),
+                        au.getPhoneNumber(), au.getCompanyName(), au.getAuthorities().stream().map(AppRole::getAuthority).collect(Collectors.joining(","))
+                )).get();
+            }
+            return null;
+        }).join();
     }
 
     @Override
     public AuthenticationResponse generateTokenForUser(AuthenticationRequest authReq) {
+        return asyncTaskExecutor.submitCompletable(() -> {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authReq.username(), authReq.password()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String token = tokenUtils.generateToken(authentication);
         log.debug("Token granted: {}", token);
-        return new AuthenticationResponse(token, true);
+        return new AuthenticationResponse(token, true); }).join();
     }
 
     @Override
     public Optional<AppUserDto> registerAppUser(AppUser appUser) {
+        return asyncTaskExecutor.submitCompletable(() -> {
         Optional<JpaUser> jpaUser = Optional.ofNullable(authServiceGateway.registerCustomerAccount(appUser));
-        return jpaUser.map(this::fromJpaUserToAppUserDto);
+        return jpaUser.map(this::fromJpaUserToAppUserDto); }).join();
     }
 
     @Override
     public Optional<AppUserDto> approveAppUser(Long accountId) {
-        Optional<JpaUser> jpaUser = Optional.ofNullable(authServiceGateway.approveCustomerAccount(accountId));
-        return jpaUser.map(this::fromJpaUserToAppUserDto);
+        return asyncTaskExecutor.submitCompletable(() -> {
+            Optional<JpaUser> jpaUser = Optional.ofNullable(authServiceGateway.approveCustomerAccount(accountId));
+            return jpaUser.map(this::fromJpaUserToAppUserDto);
+        }).join();
     }
 
     @Override
     public AuthenticationResponse checkIsValidToken(String token) {
-        return new AuthenticationResponse(token, tokenUtils.isValidToken(token));
+        return asyncTaskExecutor.submitCompletable(() -> new AuthenticationResponse(token, tokenUtils.isValidToken(token))).join();
     }
 
     private AppUserDto fromJpaUserToAppUserDto(JpaUser au) {
